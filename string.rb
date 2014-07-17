ce1250003e6bcc4d2b06558a99b9d2e390d79f78
gangelo <web.gma@gmail.com>
2014-07-17 10:23:02 -0400

    Remove some commented out code.

--- string.rb
+++ string.rb
@@ -10,23 +10,4 @@ class String
   def compress
     self.byteslice(1...-1).downcase.chars.sort.join.squeeze.length
   end
-
-=begin
-  protected
-
-  # This method simply makes sure we're dealing with an object capable of
-  # being compressed properly (e.g. implements the proper methods and
-  # properties).
-  #
-  def duck?
-    instance_of? :string
-  end
-
-  # We basically need to be dealing with a string object, so if we are, just
-  # return self, otherwise, convert us to a string object.
-  #
-  def to_duck
-    duck? ? self : self.to_s
-  end
-=end
 end
